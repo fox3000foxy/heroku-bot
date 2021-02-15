a62fb2c05fe240a9360473b13a47bff3d8102500
fox3000foxy <lucas.jl.sannier@gmail.com>
2021-02-15 13:02:33 +0100

    init

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const http = require('http')
 const bot = mineflayer.createBot({
   host: 'fox3000foxy.aternos.me', // optional
   port: 25565,       // optional
-  username: 'Tester', // email and password are required only for
-})
-
-bot.on('chat', function (username, message) {
-  if (username === bot.username) return
-  bot.chat(message)
+  username: 'IRC', // email and password are required only for
 })
 
 http.createServer((request,response)=>{
